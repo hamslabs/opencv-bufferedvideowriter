b8f4c50be830a410a8011c3d96fc5300f452a919
Alessandro Roat <alexroat@gmail.com>
2024-03-29 10:07:04 +0100

    fixed termination issue

--- BufferedVideoWriter.py
+++ BufferedVideoWriter.py
@@ -11,23 +11,27 @@ class BufferedVideoWriter(cv2.VideoWriter):
     def start(self):
         if self.thread:
             return
-        dt=1/self.fps
+        dt=1./self.fps
         def loop():
             t=time.time()
             while self.thread or self.queue.qsize():
                 frame,ts=self.queue.get()
+                if frame is None:
+                    break
                 while t<ts:
                     cv2.VideoWriter.write(self,frame)
                     t+=dt
+            self.release()
         self.thread=threading.Thread(target=loop)
         self.thread.start()
     def stop(self):
         if not self.thread:
             return
+        self.write(None)
         t=self.thread
         self.thread=None
         t.join()
-        self.release()
+        
     def write(self,image):
         if self.thread and self.queue.qsize()<self.maxBufferSize:
             return self.queue.put((image,time.time()))
--- example.py
+++ example.py
@@ -24,7 +24,7 @@ def main():
         
         if not rec:
             h,w,_=frame.shape
-            rec=BufferedVideoWriter("test.mp4",cv2.VideoWriter_fourcc(*'mp4v'), 5, (w, h))
+            rec=BufferedVideoWriter("test.mp4",cv2.VideoWriter_fourcc(*'mp4v'), 15, (w, h))
             rec.start()
 
         if rec:
@@ -37,11 +37,12 @@ def main():
         if cv2.waitKey(1) == 27:
             break
         
-    rec.stop()
+    
 
     # Release resources
     cap.release()
     cv2.destroyAllWindows()
+    rec.stop()
 
 if __name__ == "__main__":
     main()
