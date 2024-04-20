e000c52509d987657306d49cb425e9e54a724f82
Alessandro Roat <alexroat@gmail.com>
2024-04-20 19:18:36 +0200

    thread safe version of buffered video writer + requirements.txt

--- BufferedVideoWriter.py
+++ BufferedVideoWriter.py
@@ -1,19 +1,21 @@
 import cv2
 import threading,queue,time
 
-class BufferedVideoWriter(cv2.VideoWriter):
+class BufferedVideoWriter:
     def __init__(self,filename,fourcc,fps,frameSize,maxBufferSize=25):
-        cv2.VideoWriter.__init__(self,filename=filename,fourcc=fourcc,fps=fps,frameSize=frameSize)
+        self.fourcc=fourcc
         self.thread=None
         self.queue=queue.Queue()
         self.fps=fps
         self.maxBufferSize=maxBufferSize
         self.frameSize=frameSize
+        self.filename=filename
     def start(self):
         if self.thread:
             return
         dt=1./self.fps
         def loop():
+            writer=cv2.VideoWriter(filename=self.filename,fourcc=self.fourcc,fps=self.fps,frameSize=self.frameSize)
             t=time.time()
             self.queue.put((cv2.rectangle(self.frameSize, (0, 0), self.frameSize, (0, 0, 0), -1),t))
             while self.thread or self.queue.qsize():
@@ -21,9 +23,9 @@ class BufferedVideoWriter(cv2.VideoWriter):
                 if frame is None:
                     break
                 while t<ts:
-                    cv2.VideoWriter.write(self,frame)
+                    writer.write(frame)
                     t+=dt
-            self.release()
+            writer.release()
         self.thread=threading.Thread(target=loop)
         self.thread.start()
     def stop(self):
